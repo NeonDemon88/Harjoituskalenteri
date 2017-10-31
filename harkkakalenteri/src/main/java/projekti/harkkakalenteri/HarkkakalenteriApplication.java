5cd07724b581a785c573985bcdfb244fc7fd694f
NeonDemon88 <a1500957@myy.haaga-helia.fi>
2017-10-31 09:31:26 +0200

    First commit

---
+++ harkkakalenteri/src/main/java/projekti/harkkakalenteri/HarkkakalenteriApplication.java
@@ -0,0 +1,12 @@
+package projekti.harkkakalenteri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarkkakalenteriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarkkakalenteriApplication.class, args);
+	}
+}
